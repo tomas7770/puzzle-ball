8cdad504cb9121e11784ebfee5d306fe0b338020
tomas7770 <77364520+tomas7770@users.noreply.github.com>
2022-10-23 12:34:27 +0100

    Add player "friction" and speed limit

--- Scenes/PlayerBall/PlayerBall.gd
+++ Scenes/PlayerBall/PlayerBall.gd
@@ -1,9 +1,19 @@
 extends RigidBody
 
 var acceleration = 20
+var max_speed = 10
 
-func _physics_process(_delta):
+func _physics_process(delta):
+	angular_damp = -1
 	if Input.is_action_pressed("plr1_left"):
-		add_central_force(Vector3(-acceleration, 0, 0))
+		_apply_movement_force(-1, delta)
 	elif Input.is_action_pressed("plr1_right"):
-		add_central_force(Vector3(acceleration, 0, 0))
+		_apply_movement_force(1, delta)
+	else:
+		angular_damp = 15
+
+func _apply_movement_force(dir, delta):
+	# Hacky condition to limit velocity only for player-controlled movement
+	if abs(linear_velocity.x) < max_speed:
+		var real_accel = sign(dir)*min(acceleration, (max_speed - abs(linear_velocity.x))/delta)
+		add_central_force(Vector3(real_accel, 0, 0))
