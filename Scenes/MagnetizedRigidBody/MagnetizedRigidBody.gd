e6b6b3601b3712280e0d36c9c84d0dbf6daa86c2
tomas7770 <77364520+tomas7770@users.noreply.github.com>
2022-12-01 12:57:37 +0000

    Tweak magnet mechanic

--- Scenes/MagnetizedRigidBody/MagnetizedRigidBody.gd
+++ Scenes/MagnetizedRigidBody/MagnetizedRigidBody.gd
@@ -2,6 +2,12 @@ extends RigidBody
 
 var magnet_force = 30
 
+func apply_anti_gravity():
+	add_central_force(Vector3.UP*weight*gravity_scale)
+
 func apply_magnet_forces(dist_vector: Vector3, magnet_safe_distance):
-	if dist_vector.length() > magnet_safe_distance:
-		add_central_force(dist_vector.normalized()*magnet_force)
+	var length = dist_vector.length()
+	if length > magnet_safe_distance:
+		var factor = min(pow(length/8.0, 2.0), 1.0)
+		add_central_force(dist_vector.normalized()*magnet_force*factor)
+	apply_anti_gravity()
