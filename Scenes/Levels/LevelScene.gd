37e97efb20afa059e2007512e6cd209350179fd4
tomas7770 <77364520+tomas7770@users.noreply.github.com>
2023-03-20 11:08:45 +0000

    LevelScene: extract "connect child signal" method

--- Scenes/Levels/LevelScene.gd
+++ Scenes/Levels/LevelScene.gd
@@ -12,9 +12,12 @@ func _ready():
 
 func _listen_to_child_signals(child: Node):
 	if child.has_signal("plr_entered_endportal"):
-		var status = child.connect("plr_entered_endportal", self, "_finish_level")
-		if status != OK:
-			print("Connect plr_entered_endportal signal: Error " + str(status))
+		_connect_child_signal(child, "plr_entered_endportal", self, "_finish_level")
+
+func _connect_child_signal(child, signal_name, target, method, binds = [], flags = 0):
+	var status = child.connect(signal_name, target, method, binds, flags)
+	if status != OK:
+		print("Connect " + signal_name + " signal: Error " + str(status))
 
 func _finish_level():
 	# warning-ignore:return_value_discarded
