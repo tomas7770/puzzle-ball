f43b19b71f6c49687f7f943795380420cc7d3331
tomas7770 <77364520+tomas7770@users.noreply.github.com>
2023-04-02 23:18:27 +0000

    Add level select

--- Scenes/UI/MainMenu/MainMenu.gd
+++ Scenes/UI/MainMenu/MainMenu.gd
@@ -1,12 +1,33 @@
 extends Control
 
+const MAX_LEVEL = 5
+
+onready var level_label = $LevelSelect/Label
+var selected_level = 1
+
 func _ready():
 	$Buttons/PlayButton.grab_focus()
+	_update_level_label()
 
 func _on_PlayButton_pressed():
-	var status = LevelManager.start_level("Level1")
+	var status = LevelManager.start_level("Level" + str(selected_level))
 	if status != OK:
 		print(status)
 
 func _on_QuitButton_pressed():
 	get_tree().notification(MainLoop.NOTIFICATION_WM_QUIT_REQUEST)
+
+func _update_level_label():
+	level_label.text = "Level " + str(selected_level)
+
+func _on_PrevLvlButton_pressed():
+	selected_level -= 1
+	if selected_level < 1:
+		selected_level = MAX_LEVEL
+	_update_level_label()
+
+func _on_NextLvlButton_pressed():
+	selected_level += 1
+	if selected_level > MAX_LEVEL:
+		selected_level = 1
+	_update_level_label()
