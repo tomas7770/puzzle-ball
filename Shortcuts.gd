7cf6c441afc7d4c17bd6f4f8fb283c12e6f42884
tomas7770 <77364520+tomas7770@users.noreply.github.com>
2023-07-29 20:47:52 +0000

    Fix fullscreen shortcut not working when paused

--- Shortcuts.gd
+++ Shortcuts.gd
@@ -1,5 +1,8 @@
 extends Node
 
+func _ready():
+	pause_mode = Node.PAUSE_MODE_PROCESS
+
 func _input(event):
 	if event.is_action_pressed("toggle_fullscreen"):
 		OS.set_window_fullscreen(!OS.window_fullscreen)
