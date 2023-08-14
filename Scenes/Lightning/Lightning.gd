6923a672e628ab7006e3aaaaa1de6288620cbc1a
tomas7770 <77364520+tomas7770@users.noreply.github.com>
2023-08-14 17:25:22 +0000

    Enhance lightning beam

--- Scenes/Lightning/Lightning.gd
+++ Scenes/Lightning/Lightning.gd
@@ -15,7 +15,7 @@ func _process(_delta):
 	var pos2 = edge_obj2.get_global_transform_interpolated().origin
 	translation = pos1.linear_interpolate(pos2, 0.5)
 	look_at(pos2, (pos2-translation).cross(Vector3(0,0,1)))
-	mesh_instance.mesh.size = Vector2(pos1.distance_to(pos2), 1.0)
+	mesh_instance.mesh.size = Vector2(pos1.distance_to(pos2), 2.0)
 
 func set_edge_obj1(obj):
 	edge_obj1 = obj
